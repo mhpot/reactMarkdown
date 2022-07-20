5ea3500166ea747eb3f451327bbbce985a2b12ed
Pierce Boyack <piercebo@buffalo.edu>
2022-07-19 23:17:23 -0400

    Updates

---
+++ precache-manifest.ea4cf250f95ed494a78c140e0fa539f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be0462c4a9fa57d353d1",
+    "url": "/reactMarkdown/static/css/main.8162cc2c.chunk.css"
+  },
+  {
+    "revision": "be0462c4a9fa57d353d1",
+    "url": "/reactMarkdown/static/js/main.2893bc20.chunk.js"
+  },
+  {
+    "revision": "11ab7c42837674e905aa",
+    "url": "/reactMarkdown/static/js/runtime~main.54754250.js"
+  },
+  {
+    "revision": "5f144272b78b150d7ace",
+    "url": "/reactMarkdown/static/js/2.e889f31b.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/reactMarkdown/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "6354e4e3306c3c42433239746e5cb2bd",
+    "url": "/reactMarkdown/index.html"
+  }
+];
